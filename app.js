5dda5fde2bd668a91aecfdab2201fb9dca3d9637
Emil Eremiev <eeremiev@usuario-lt.chequemotiva.local>
2018-08-13 14:49:58 +0200

    fix

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use((req, res, next) => {
-  if (req.path === '/api/upload' || req.path === '/api/trader' || req.path === '/api/robots/*') {
+  if (req.path === '/api/upload' || req.path === '/api/trader' || req.path.substr(0, 11) === '/api/robots') {
     next();
   } else {
     lusca.csrf()(req, res, next);
