83b057520b0566dc7eeabd2bd3d7953b71280a03
eremiev <emil.eremiev@gmail.com>
2018-08-15 02:52:17 +0300

    m

--- controllers/robot.js
+++ controllers/robot.js
@@ -94,10 +94,10 @@ exports.putRobots = async (req, res, next) => {
       user.robots.forEach(async (robot) => {
         if (robot.robot_id === messageObj.id) {
           if (robot.account_number === messageObj.account_id) {
-            user.robots.pull({ _id: robot.id });
-            robot.last_active = Date.now();
-            user.robots.push(robot);
-            user.save((err) => { if (err) { console.log('1');return next(err); } });
+            // user.robots.pull({ _id: robot.id });
+            // robot.last_active = Date.now();
+            // user.robots.push(robot);
+            // user.save((err) => { if (err) { return next(err); } });
 
             await Live.findOne({ robot_id: robot.robot_id }, (err, liveRobot) => {
               if (err) { return next(err); }
@@ -113,13 +113,13 @@ exports.putRobots = async (req, res, next) => {
                   }
                   liveRobot.pending_action = null;
                   liveRobot.pending_time = null;
-                  liveRobot.save((err) => { if (err) { console.log('2'); return next(err); } });
+                  liveRobot.save((err) => { if (err) { return next(err); } });
                 }
               } else {
                 const live = new Live();
                 live.robot_id = robot.robot_id;
                 live.symbol = robot.symbol;
-                live.save((err) => { if (err) { console.log('3'); return next(err); } });
+                live.save((err) => { if (err) { return next(err); } });
               }
             });
             res.status(200).send(action);
